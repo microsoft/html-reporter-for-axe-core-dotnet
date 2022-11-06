ac301dc3cbca3feee6d924f0e7b26375624e3817
IsaacWalker <isaacwalker1377@gmail.com>
2022-11-06 17:03:54 +0000

    Added inital reporter interface and models

---
+++ src/html-reporter/AxeHTMLReporter.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AxeCore.HTMLReporter
+{
+    /// <inheritdoc />
+    public sealed class AxeHTMLReporter : IAxeHTMLReporter
+    {
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static AxeHTMLReporter Instance { get; } = new AxeHTMLReporter();
+
+        internal AxeHTMLReporter()
+        {
+        }
+
+        /// <inheritdoc />
+        public AxeHTMLReport CreateReport(object results, AxeHTMLReportOptions options = null)
+        {
+            // TODO IsaacWalker - Implement Report Generation
+            return new AxeHTMLReport("<a> Hello World </a>");
+        }
+    }
+}
---
+++ src/html-reporter/IAxeHTMLReporter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AxeCore.HTMLReporter
+{
+    /// <summary>
+    /// Reporter for generting HTML Report of an axe-core run result.
+    /// </summary>
+    public interface IAxeHTMLReporter
+    {
+        /// <summary>
+        /// Creates a HTML Report for an axe-core run result.
+        /// </summary>
+        /// <param name="results">The axe-core results. TODO IsaacWalker - integrate with https://github.com/dequelabs/axe-core-nuget/blob/develop/packages/commons/src/AxeResult.cs</param>
+        /// <param name="options">HTML Report options.</param>
+        /// <returns>The HTML report object.</returns>
+        AxeHTMLReport CreateReport(object results, AxeHTMLReportOptions options = null);
+    }
+}
