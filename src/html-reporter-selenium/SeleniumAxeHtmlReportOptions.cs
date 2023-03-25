b566edb36d1880ff987928ebe58bbf8ac1e232b1
IsaacWalker <isaacwalker1377@gmail.com>
2023-03-25 15:18:22 +0000

    Added Selenium API and test infrastructure

---
+++ src/html-reporter-selenium/SeleniumAxeHtmlReportOptions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AxeCore.HTMLReporter.Selenium
+{
+    /// <inheritdoc />
+    public sealed class SeleniumAxeHtmlReportOptions : AxeHTMLReportOptions
+    {
+        /// <summary>
+        /// Report Filename
+        /// </summary>
+        public string ReportFilename { get; set; } = null;
+    }
+}
---
+++ tests/html-reporter-selenium/IntegrationTests.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Deque.AxeCore.Commons;
+using Deque.AxeCore.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace AxeCore.HTMLReporter.Selenium.Tests
+{
+    [TestFixture]
+    [Category("Integration")]
+    public sealed class IntegrationTests
+    {
+        private IWebDriver m_webDriver;
+
+        public IntegrationTests()
+        {
+            DriverManager driverManager = new();
+            driverManager.SetUpDriver(new ChromeConfig());
+        }
+
+        [SetUp]
+        public void SetupTest()
+        {
+            m_webDriver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            m_webDriver.Close();
+        }
+
+        [Test]
+        public void InvokingAnalyze_WithHtmlReport_ShouldGenerateHTMLReportFile()
+        {
+            NavigateToFile();
+
+            AxeBuilder builder = new AxeBuilder(m_webDriver)
+                .WithHTMLReport();
+
+            AxeResult result = builder.Analyze();
+
+            Assert.IsNotNull(result);
+
+            // TODO IsaacWalker - Verify HTML Report file is present.
+        }
+
+        private void NavigateToFile(string file = "default-test-page.html")
+        {
+            string fullFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", file);
+
+            if (!File.Exists(fullFilePath))
+            {
+                throw new FileNotFoundException($"File not found at {fullFilePath}");
+            }
+
+            Uri url = new($"file://{fullFilePath}");
+
+            m_webDriver.Navigate().GoToUrl(url);
+        }
+    }
+}
