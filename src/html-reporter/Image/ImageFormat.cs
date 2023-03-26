be030519644afe8dd871d679262ae76bcfdc44b7
IsaacWalker <isaacwalker1377@gmail.com>
2023-03-26 19:57:40 +0100

    Formatted

---
+++ src/html-reporter/Image/ImageFormat.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace AxeCore.HTMLReporter.Image
+{
+    /// <summary>
+    /// Supported Image Formats
+    /// </summary>
+    public enum ImageFormat
+    {
+        /// <summary>
+        /// JPeg Image Format
+        /// </summary>
+        JPeg,
+
+        /// <summary>
+        /// Png Image Format
+        /// </summary>
+        Png
+    }
+}
---
+++ tests/html-reporter/ImageUtilsTests.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using AxeCore.HTMLReporter.Image;
+using NUnit.Framework;
+using System;
+
+namespace AxeCore.HTMLReporter.Tests
+{
+    [TestFixture]
+    public sealed class ImageUtilsTests
+    {
+        [Test]
+        public void CreateDataUri_WithInputData_ShouldReturnExpectedDataUri()
+        {
+            byte[] inputData = new byte[] { 1, 2, 3, 4 };
+
+            Uri expectedUri = new Uri("data:image/jpeg;base64,AQIDBA==");
+            Uri actualDataUri = ImageUtils.CreateDataUri(inputData, ImageFormat.JPeg);
+
+            Assert.AreEqual(expectedUri, actualDataUri);
+        }
+    }
+}
